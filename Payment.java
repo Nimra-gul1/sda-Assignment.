685b733cec3a321b9e3808d6c7cd31bb8ea0d612
Nimra-gul1 <nimraofficial121@gmail.com>
2025-03-05 17:20:02 +0500

    follows srp now

--- Payment.java
+++ Payment.java
@@ -1,6 +1,7 @@
 public class Payment {
     public static boolean processPayment(Customer customer, double amount) {
-        // Logic for processing a payment
+        // Before SRP Fix: Payment logic was inside Order.
+        // SRP Fix: Payment logic is now separate.
         System.out.println("Processed payment of $" + String.format("%.2f", amount) + " for " + customer.getUsername());
         return true;
     }
