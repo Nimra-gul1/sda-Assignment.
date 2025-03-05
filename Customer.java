ca97b01eddef5aa1fe52a8a02a9ddd4ba467b9d7
Nimra-gul1 <nimraofficial121@gmail.com>
2025-03-05 17:14:02 +0500

    it follows srp now

--- Customer.java
+++ Customer.java
@@ -1,5 +1,5 @@
 public class Customer extends User {
-    private String email;
+    private final String email; 
 
     public Customer(String username, String password, String email) {
         super(username, password);
@@ -10,8 +10,12 @@ public class Customer extends User {
         return email;
     }
 
-    public void purchaseBook(Book book) {
-        // Logic for purchasing a book
-        System.out.println(getUsername() + " purchased " + book.getTitle());
-    }
+    // Before SRP Violation:
+    // The Customer class was responsible for both storing user details and handling book purchases.
+    // This violated the Single Responsibility Principle because a class should only have one reason to change.
+
+    // After SRP Resolution:
+    // Now, the Customer class only manages customer-related data (username, password, email).
+    // The book purchasing logic has been removed.
+    // Purchasing functionality should now be handled by a separate class like PaymentProcessor.
 }
