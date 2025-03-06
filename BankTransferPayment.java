64f7bb45b8c8d1be991c0ff3f13e9d2bbdf909db
MaryammSafdar <46481@students.riphah.edu.pk>
2025-03-06 18:51:10 +0500

    lps completed

--- BankTransferPayment.java
+++ BankTransferPayment.java
@@ -1,9 +1,19 @@
-
 public class BankTransferPayment implements PaymentMethod {
 
     @Override
     public boolean processPayment(Customer customer, double amount) {
-        System.out.println("Processed payment of $" + String.format("%.2f", amount) + " via Bank Transfer for " + customer.getUsername());
+        // LSP is followed: This class can be used wherever PaymentMethod is expected
+        // OCP is followed: We can introduce new payment methods without modifying existing ones
+        // SRP is followed: This class is only responsible for processing bank transfer payments
+
+        // Simulating a bank transfer process
+        if (amount <= 0) {
+            System.out.println("Invalid payment amount. Transaction failed.");
+            return false;
+        }
+
+        System.out.println("Processed payment of $" + String.format("%.2f", amount) + 
+                           " via Bank Transfer for " + customer.getUsername());
         return true;
     }
 }
